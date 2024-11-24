1a1dad03dffbb30d76a8587c0e0b0d8f30ed46a5
BethTesHS <131249570+BethTesHS@users.noreply.github.com>
2024-11-24 22:02:19 +0300

    Task files creates

---
+++ Task_1.asm
@@ -0,0 +1,11 @@
+/*
+1. Control Flow and Conditional Logic (6 Marks) 
+    a. Write a program that: 
+        i. Prompts for a user input number. 
+        ii. Uses branching logic to classify the number as “POSITIVE,” 
+            “NEGATIVE,” or “ZERO.” 
+        iii. Use both conditional and unconditional jumps in the program 
+            to handle these cases effectively. 
+    b. Documentation Requirement: In your comments, explain why you chose 
+        specific jump instructions and how each one impacts the program flow
+*/
